21119f2fa956f21c7f623783d977554c8510c2fd
kralluz <chenrique201520@gmail.com>
2024-11-02 21:03:59 -0300

    a

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,9 @@
-const CLIENT_ID = "1000.NWRY2C51S8GH236LC9T9F7EJL3D1ZD";
-const REDIRECT_URI =
-  "https://3bf7-2804-2ee8-82-c8c6-414f-cf2f-f426-961e.ngrok-free.app"; // Redireciona para o backend
+const CLIENT_ID = "1000.XH2XF1GALICCH7P3E708N0AFT22TBB";
+const REDIRECT_URI = 'https://3bf7-2804-2ee8-82-c8c6-414f-cf2f-f426-961e.ngrok-free.app/callback'; // Redireciona para o backend
 
 const Login = () => {
   const handleLogin = () => {
-    const authUrl = `https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=${CLIENT_ID}&scope=ZohoBigin.modules.ALL&access_type=offline&redirect_uri=${encodeURIComponent(
-      REDIRECT_URI
-    )}`;
+    const authUrl = `https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=${CLIENT_ID}&scope=ZohoBigin.modules.ALL&access_type=offline&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
     window.location.href = authUrl;
   };
 
@@ -14,8 +11,12 @@ const Login = () => {
     <div>
       <h1>Integração OAuth 2.0 com Bigin</h1>
       <button onClick={handleLogin}>Login com Bigin</button>
+
     </div>
   );
 };
+
+
+
 
 export default Login;
