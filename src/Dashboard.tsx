e2e9c795e3e8699dcd0eda0a5738485ede8712f2
kralluz <chenrique201520@gmail.com>
2024-11-02 21:13:04 -0300

    a+++++++

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,6 @@
 // src/Dashboard.js
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
